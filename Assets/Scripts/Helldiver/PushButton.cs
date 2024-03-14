424b0964f8ab181dcc8dc27b0aaa8f4f6ca67ffe
Hugo Caroumbin | "RatonFuté <jeromehugo1@hotmail.fr>
2024-03-14 09:45:33 +0100

    button done

--- Assets/Scripts/Helldiver/PushButton.cs
+++ Assets/Scripts/Helldiver/PushButton.cs
@@ -10,4 +10,14 @@ public class PushButton : MonoBehaviour
 {
     public Action<BTType> OnButtonPressed;
     public BTType type;
+    [SerializeField] private AudioSource _audio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 7)
+        {
+            OnButtonPressed?.Invoke(type);
+            if(_audio != null)  _audio.Play();
+        }
+    }
 }
